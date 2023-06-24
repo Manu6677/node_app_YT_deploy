d2b1722b0df9ee5a495e3e8867ad781bc0080c3a
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-06-24 17:52:57 +0530

    second ch

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-const lib = require("./lib.js");
+const http = require("http");
 const fs = require("fs");
 
-const t1 = performance.now();
+const index = fs.readFileSync("index.html", "utf-8");
 
-// USE this it is a asynchronous process and does not bloack the server
-fs.readFile("demo.txt", "utf-8", (err, txt) => {
-  console.log(txt);
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const prd = products.find((p) => p.id === +id); //Rem. no curly braces on p.id === +id
+    // and +id is + covert from string to number
+    res.setHeader("Content-Type", "text/html");
+    let modifiedIndex = index
+      .replace("**title**", prd.title)
+      .replace("**price**", prd.price)
+      .replace("**url**", prd.thumbnail)
+      .replace("**rating**", prd.rating);
+    res.end(modifiedIndex);
+
+    return;
+  }
+
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(data));
+      break;
+    case "/product":
+      res.setHeader("Content-Type", "text/html");
+      let modifiedIndex = index
+        .replace("**title**", products.title)
+        .replace("**price**", products.price)
+        .replace("**url**", products.thumbnail)
+        .replace("**rating**", products.rating);
+      res.end(modifiedIndex);
+      break;
+    default:
+      res.writeHead(404, "NT FOUND");
+      res.end();
+  }
+
+  console.log("server started");
+  // res.setHeader("Content-Type", "text/html");
+  // res.setHeader("Content-Type", "application/json");
+  // res.end(index);
 });
 
-/**
- -Do not use fs.readFileSync because it block the server, it is synchronous process
-
-   -const txt = fs.readFileSync("demo.txt", "utf-8");
-   -console.log(txt);
- */
-
-console.log(lib);
-console.log(lib.sum(4, 6), lib.diff(10, 9));
-
-const t2 = performance.now();
-
-console.log(t2 - t1);
+server.listen(8080);
