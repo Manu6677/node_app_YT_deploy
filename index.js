de8200946d03eac7d7a93487722a5e59d452bb13
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-06-26 20:26:46 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,56 +1,63 @@
 const express = require("express");
 const fs = require("fs");
-const morgan = require("morgan");
+const morgan = require("morgan"); // It is a logger
 
-const index = fs.readFileSync("index.html", "utf-8");
-
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const index = fs.readFileSync("index.html", "utf-8"); // index file read kri
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8")); // JSON data parse kia then neche fr json data m product access kia
 const products = data.products;
 
 const server = express();
 
-server.use((req, res, next) => {
-  console.log(
-    req.method,
-    req.ip,
-    req.hostname,
-    req.url,
-    new Date(),
-    req.get("User-Agent")
-  );
-  next();
-});
-
+// Very Important for body to be Parsed
 server.use(express.json());
+// To get static files if the name is index then only
 server.use(express.static("public"));
-server.use(morgan("default"));
+// Logger
+server.use(morgan("combined"));
 
-const auth = (req, res, next) => {
-  //console.log(req.query);
-  if (req.body.q === "123") {
-    next();
-  } else {
-    res.status(401).send();
-  }
-};
-
-//server.use(auth);
-server.get("/demo", auth, (req, res) => {
-  // res.send("manu");
-  //res.json(data);
+// Read GET API
+server.get("/products", (req, res) => {
   res.status(201).send(data);
 });
 
-server.post("/demo2", auth, (req, res) => {
-  res.status(201).send(data);
+// Read GET API by :id
+server.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const result = products.find((p) => p.id === +id);
+
+  res.status(200).json(result);
 });
 
-server.delete("/demo3", auth, (req, res) => {
-  res.status(201).send(data);
+server.post("/demo2", (req, res) => {
+  console.log(req.body);
+  const newProduct = products.push(req.body);
+  res.status(200).json(newProduct);
 });
 
-server.patch("/demo4", auth, (req, res) => {
-  res.status(201).send(data);
+server.put("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const productIndex = products.findIndex((p) => p.id === +id);
+  products.splice(productIndex, 1, { ...req.body, id: id });
+  res.json({ updated: "Done Successfully" });
+});
+
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+
+  const product = products[productIndex]; // Here we got the actual product object b/c of Index
+  products.splice(productIndex, 1, { ...product, ...req.body }); // Overwrite ho jaigi yaha original product with changed req.body
+  res.status(201).json({ updated: "Done" });
+});
+
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = productIndex[products];
+
+  products.splice(productIndex, 1);
+
+  res.status(201).json(product);
 });
 
 server.listen(8080, () => {
